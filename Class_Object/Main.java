bf4c4d319dd1e64edf7d2fd7468ca54dd94b4edd
mostakinahmed <mostakin4556@gmail.com>
2026-01-05 13:12:42 +0600

    first commit

--- Class_Object/Main.java
+++ Class_Object/Main.java
@@ -1,28 +1,28 @@
-package Test_2;
+package Class_Object;
 
 class MathCalculate {
 
     // sum
-    public int sum(int a, int b) {
+    public void sum(int a, int b) {
         int res = a + b;
-        return res;
+        System.out.println(res);
     }
 
     // minus
     public int subs(int a, int b) {
-        int res = a + b;
+        int res = a - b;
         return res;
     }
 
     // multiply
     public int multiply(int a, int b) {
-        int res = a + b;
+        int res = a * b;
         return res;
     }
 
     // division
     public int div(int a, int b) {
-        int res = a + b;
+        int res = a / b;
         return res;
     }
 }
@@ -31,11 +31,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("\n-----------Math-------------");
-
         MathCalculate s = new MathCalculate();
 
-        int res = s.sum(10, 20);
-        System.out.println(res);
+        s.sum(10, 20);
 
     }
 }
