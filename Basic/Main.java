f36bf6f5345c34b824ccaefb7e9b10e181fc762b
mostakinahmed <mostakin4556@gmail.com>
2026-01-16 22:00:39 +0600

    add basic

--- Basic/Main.java
+++ Basic/Main.java
@@ -1,47 +1,110 @@
 package Basic;
 
-class X {
+// class X {
 
-    void test() {
-        int[] data = { 4, 2, 8, 3, 7, 5 };
+//     void test() {
 
-        // ----------Forech------------
-        // for (int item : data) {
-        // if (item == 8) {
-        // return;
-        // }
-        // System.out.println(item);
-        // }
+//         String[] data = new String[3];
 
-        // ----------For--------------
-        // for (int i = 0; i < data.length; i++) {
-        // System.out.println(data[i]);
-        // }
+//         data[0] = "M";
+//         data[1] = "O";
+//         data[2] = "S";
+//         for (String item : data) {
+//             System.out.print(item);
+//         }
 
-        int mark = 20;
+//         // ----------Forech------------
+//         // for (int item : data) {
+//         // if (item == 8) {
+//         // return;
+//         // }
+//         // System.out.println(item);
+//         // }
 
-        // if (mark >= 40) {
-        // System.out.println("Pass");
-        // } else {
-        // System.out.println("Failled");
-        // }
+//         // ----------For--------------
+//         // for (int i = 0; i < data.length; i++) {
+//         // System.out.println(data[i]);
+//         // }
 
-        String res = (mark >= 40) ? "pass" : "Faill";
-        System.out.println("res is " + res);
+//         int mark = 20;
 
+//         // if (mark >= 40) {
+//         // System.out.println("Pass");
+//         // } else {
+//         // System.out.println("Failled");
+//         // }
+
+//         String res = (mark >= 40) ? "pass" : "Faill";
+//         System.out.println("\nres is " + res);
+
+//     }
+
+// }
+
+// // public class Main {
+// // public static void main(String[] args) {
+// // System.out.println("--------------------------------------");
+// // System.out.println("\nOutput--------");
+
+// // X obj = new X();
+// // obj.test();
+
+// // System.out.println("\n\n");
+
+// // }
+// // }
+
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("mm");
+//         // code to be executed
+//     }
+// }
+
+class Mostakin {
+
+    int id;
+    String name;
+    private String pass;
+
+    // ----------getter--------
+    String getPass() {
+        return pass;
+    }
+
+    // -----------setter---------
+    void setPass(String newPass) {
+        this.pass = newPass;
+    }
+}
+
+class Info extends Mostakin {
+
+    void display() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Password: " + getPass());
     }
 
 }
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("--------------------------------------");
-        System.out.println("\nOutput--------");
 
-        X obj = new X();
-        obj.test();
+        System.out.println("---------output----------");
+        Info obj = new Info();
+        obj.id = 831;
+        obj.name = "Mostakin Ahmed";
+        obj.setPass("abc12345");
 
-        System.out.println("\n\n");
+        Info obj2 = new Info();
+        obj2.id = 367;
+        obj2.name = "Tonmoy";
+        obj2.setPass("xyz54321");
+
+        obj.display();
+        obj2.display();
 
     }
 }
